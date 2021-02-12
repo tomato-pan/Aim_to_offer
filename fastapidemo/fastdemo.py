0efb3aa87e010c0b5809065c8dcade297dc9b82e
panjie <260313538@.com>
2021-02-12 10:45:37 +0800

    add fastdemo

--- fastapidemo/fastdemo.py
+++ fastapidemo/fastdemo.py
@@ -1,13 +1,23 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI,Form
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def read_home():
-    return {"message": "Welcome to tomato's home!"}
+async def read_home(request: Request):
+    return templates.TemplateResponse("user.html", {"request": request})
+
+
+@app.post("/user/")
+async def up_user_info(request: Request, username: str = Form(...), password: str = Form(...)):
+    print(username)
+    print(password)
+    return templates.TemplateResponse("index.html",{"request": request, "username": username, "password": password})
 
 
 if __name__ == '__main__':
