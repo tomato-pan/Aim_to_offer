87ff9f6a54ae46fc44a4d87fdc0a060a760b29a7
panjie <260313538@.com>
2021-02-16 16:59:38 +0800

    add fastdemo1-backenddemo

--- fastapidemo1/fastdemo1.py
+++ fastapidemo1/fastdemo1.py
@@ -1,7 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from enum import Enum
+from typing import Optional
 
 app = FastAPI()
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class Name(str, Enum):
+    a = "z0"
+    b = "z1"
+    c = "z2"
 
 
 @app.get("/")
@@ -10,10 +19,38 @@ async def read():
         "message": 123
     }
 
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/ab/")
+async def ab(A: int = 0, B: int = 20):
+    return {
+        "message": B + A
+    }
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/a/{name}")
+async def get_name(name: Name):
+    if name == Name.a:
+        return {"name": name, "message": name + " no!"}
+    if name == Name.b:
+        return {"name": name, "message": name + " no no!"}
+    return {"name": name, "message": name + " yes!"}
 
 
 if __name__ == '__main__':
