f7347055dee83cb0bde5f0211440bb38bea2f693
panjie <260313538@.com>
2021-02-16 16:28:26 +0800

    add fastdemo-bootstrap

--- fastapidemo/fastdemo.py
+++ fastapidemo/fastdemo.py
@@ -1,49 +1,64 @@
 from typing import Optional
 from fastapi import FastAPI, Form, File, UploadFile
 from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 from starlette.requests import Request
 from typing import List
 import uvicorn
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-# @app.post("/user/")
-# async def up_user_info(request: Request, username: str = Form(...), password: str = Form(...)):
-#     print(username)
-#     print(password)
-#     return templates.TemplateResponse("index.html", {"request": request, "username": username, "password": password})
-#
-@app.post("/files/")
-async def files(request: Request,
-                files_list: List[bytes] = File(...),
-                files_name: List[UploadFile] = File(...), ):
+@app.post("/user/")
+async def up_user_info(request: Request,
+                       username: str = Form(...),
+                       password: str = Form(...)
+                       ):
+    print(username)
+    print(password)
     return templates.TemplateResponse("index.html",
-                                      {"request": request,
-                                       "file_sizes": [len(file) for file in files_list],
-                                       "filenames": [file.filename for file in files_name]
-                                       }
-                                      )
-
-
-@app.post("/create_file/")
-async def create_file(request: Request,
-                      file: bytes = File(...),
-                      fileb: UploadFile = File(...),
-                      notes: str = Form(...)):
-    return templates.TemplateResponse("index.html",
-                                      {"request": request,
-                                       "file_size": len(file),
-                                       "notes": notes,
-                                       "fileb_content_type": fileb.content_type
-                                       }
-                                      )
+                                      {
+                                          "request": request,
+                                          "username": username
+                                      })
 
 
 @app.get("/")
 async def main(request: Request):
-    return templates.TemplateResponse("filepost.html", {"request": request})
+    return templates.TemplateResponse("signin.html", {"request": request})
+
+
+# @app.post("/files/")
+# async def files(request: Request,
+#                 files_list: List[bytes] = File(...),
+#                 files_name: List[UploadFile] = File(...), ):
+#     return templates.TemplateResponse("index1.html",
+#                                       {"request": request,
+#                                        "file_sizes": [len(file) for file in files_list],
+#                                        "filenames": [file.filename for file in files_name]
+#                                        }
+#                                       )
+#
+#
+# @app.post("/create_file/")
+# async def create_file(request: Request,
+#                       file: bytes = File(...),
+#                       fileb: UploadFile = File(...),
+#                       notes: str = Form(...)):
+#     return templates.TemplateResponse("index1.html",
+#                                       {"request": request,
+#                                        "file_size": len(file),
+#                                        "notes": notes,
+#                                        "fileb_content_type": fileb.content_type
+#                                        }
+#                                       )
+
+
+# @app.get("/")
+# async def main(request: Request):
+#     return templates.TemplateResponse("filepost.html", {"request": request})
 
 
 if __name__ == '__main__':
