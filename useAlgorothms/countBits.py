38097424f04440d0ddae8ec5e3cc646d71cc8582
panj <pj_uestc@163.com>
2021-03-04 10:31:42 +0800

    add countBits

--- useAlgorothms/countBits.py
+++ useAlgorothms/countBits.py
@@ -16,15 +16,28 @@
 class Solution:
     def countBits(self, num: int) -> list:
         bit_list=[]
-        if num<0:return []
-        elif num==0:return [0]
-        else:
-            for i in range(0,num+1):
-                i = str(bin(i))
-                bit_list.append(i.count("1"))
-        print(bit_list)
+        for i in range(num+1):
+            bit_list.append(bin(i).count("1"))
         return bit_list
+    # 动态规划
+    def countBits1(self, num: int) ->list:
+        bits = [0]
+        highBit = 0
+        for i in range(1, num + 1):
+            if i & (i - 1) == 0:
+                highBit = i
+            bits.append(bits[i - highBit] + 1)
+        return bits
+
+
 
 if __name__ == '__main__':
+    import time
+    t1 =time.time()
     s = Solution()
-    s.countBits(5)
+    # s.countBits(10021321)
+    # t2=time.time()
+    # print(t2-t1)
+    # s.countBits1(10021321)
+    # print(time.time()-t2)
+    print(100&99)
