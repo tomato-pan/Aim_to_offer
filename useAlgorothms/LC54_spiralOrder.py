8d78d339a630463c91265887b4f95962299f1066
panj <pj_uestc@163.com>
2021-03-15 11:31:25 +0800

    add 54

---
+++ useAlgorothms/LC54_spiralOrder.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:return []
+        M, N = len(matrix), len(matrix[0])
+        left, right, up, down = 0, N - 1, 0, M - 1
+        res = []
+        x, y = 0, 0
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        cur_d = 0
+        while len(res) != M * N:
+            res.append(matrix[x][y])
+            if cur_d == 0 and y == right:
+                cur_d += 1
+                up += 1
+            elif cur_d == 1 and x == down:
+                cur_d += 1
+                right -= 1
+            elif cur_d == 2 and y == left:
+                cur_d += 1
+                down -= 1
+            elif cur_d == 3 and x == up:
+                cur_d += 1
+                left += 1
+            cur_d %= 4
+            x += dirs[cur_d][0]
+            y += dirs[cur_d][1]
+        return res
+
+
+if __name__ == '__main__':
+    s =Solution()
+    print(s.spiralOrder(matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
