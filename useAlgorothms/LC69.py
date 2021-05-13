57144825700ee5b2a9c93b3bebd0a161f72d058c
panj <260313538@qq.com>
2021-05-13 10:04:59 +0800

    add learning

--- useAlgorothms/LC69.py
+++ useAlgorothms/LC69.py
@@ -6,7 +6,10 @@ class Solution:
             prev = res
             res = (res + x / res) / 2
         return int(res)
-
+    def x(self,s1,s2):
+        if s2 not in s1:return-1
+        else: return s1.index(s2)
 if __name__ == '__main__':
     s = Solution()
     print(s.mySqrt(8))
+    print(s.x("hello","ll"))
