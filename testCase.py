90c2612241fe5a072820b793d4bafd92f9a72ffd
panj <pj_uestc@163.com>
2021-06-23 13:46:20 +0800

    add: python+robotframework(pure python code)

---
+++ testCase.py
@@ -0,0 +1,9 @@
+from robot.api import TestSuite
+
+suite = TestSuite('Activate Skynet')
+suite.resource.imports.library('OperatingSystem')
+test = suite.tests.create('Should Activate Skynet', tags=['smoke'])
+test.keywords.create('Set Environment Variable', args=['SKYNET', 'activated'])
+test.keywords.create('Environment Variable Should Be Set', args=['SKYNET'])
+print(test)
+# suite.run()
