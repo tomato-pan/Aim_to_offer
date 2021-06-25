d6fc70b3a0d5006ef910cee0f92d296955214766
panjie <260313538@.com>
2021-06-25 08:03:38 +0800

    add mapDemo

---
+++ demo/mapDemo.py
@@ -0,0 +1,70 @@
+import requests
+import re
+from urllib import parse
+import time, random
+
+with open(r"C:\Users\95\Desktop\world_coordinate.txt", "r", encoding="utf-8") as f:
+    aaa = f.read()
+
+
+def find_area(text):
+    center_content1 = re.findall(r'<meta content="(.*?)" itemprop="image">', text)
+    print(center_content1)
+    if center_content1 != []:
+        center_content = center_content1[0]
+        if "center" in center_content:
+            center = parse.unquote(re.findall(r'center=(.*?)&amp;', center_content)[0])
+        else:
+            center = "查找不到"
+    else:
+        center = "查找不到"
+    # print(center_content)
+    print(center)
+    return center
+
+
+def rand_ua():
+    Ua = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+          "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+          "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+          "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+          "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+          "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+          "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+          "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+          "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+          "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+          "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"]
+    return Ua[random.randint(0, len(Ua) - 1)]
+
+
+def get_center():
+    num = 0
+    for i in aaa.split("\n"):
+        print(i)
+        num += 1
+        if num % 100 == 0:
+            num = 0
+            time.sleep(200)
+        header = {
+            "User-Agent": rand_ua()}
+        url = "https://www.google.com/maps/search/" + i + "/@"
+        time.sleep(3)
+        res = requests.get(url, headers=header)
+        # print(res.text)
+        center = find_area(res.text)
+        with open("D:\google_map.txt", "a+", encoding="utf-8") as f:
+            string = i + " " + center + "\n"
+            f.write(string)
+
+
+if __name__ == '__main__':
+    get_center()
+    # print(rand_ua())
