d56e4824fc1139cf9946e4e3e1b06c98abbc63d6
panj <pj_uestc@163.co>
2021-11-30 10:56:16 +0800

    add: decorator

--- fundationDemo/closures.py
+++ fundationDemo/closures.py
@@ -5,5 +5,26 @@ def print_msg(msg):
     return printer()
 
 
+"""装饰器：装饰器带参数"""
+
+
+def my_decorator(name):
+    def outer(func):
+        def inner(*args, **kwargs):
+            print("********")
+            print("添加带装饰器参数%s的功能代码" % name)
+            func(*args, **kwargs)
+
+        return inner
+
+    return outer
+
+
+@my_decorator(name='settings')
+def script3(arg):
+    print("测试----%s" % arg)
+
+
 if __name__ == '__main__':
     print_msg("message!")
+    script3("bbb")
