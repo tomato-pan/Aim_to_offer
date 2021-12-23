f90a153f79297e86dad2b7a3b1f2451fa24b7437
panj <pj_uestc@163.com>
2021-12-23 10:22:14 +0800

    add: tcp_mock

--- useAlgorothms/LC5.py
+++ useAlgorothms/LC5.py
@@ -57,15 +57,30 @@ class Solution:
                         start = i
 
         return s[start:start + max_len]
+    
+    def longestPalindrome3(self, s: str) -> str:
+        res =""
+        for i in range(len(s)):
+            s1 = self.palindrome(s,i,i)
+            s2 = self.palindrome(s,i,i+1)
+            res=res if len(res)>len(s1) else s1
+            res=res if len(res)>len(s2) else s2
+        # print(res)
+        return res
 
+    def palindrome(self,s,l,r):
+        while(l>=0 and r<len(s) and s[l]==s[r]):
+            l-=1
+            r+=1
+        return s[l+1:r]
 
 if __name__ == '__main__':
     t1 = time.time()
     aa = "ibawpzhrunsgfobmenlqlxnprtgijgbeicsuoihnmcekzmvtffmlpzuwlimuuzjhkzppmpqqrfwyrjrsltkypjpcjffpvhtdiwjdonutobpecsiqubiusvwsyhrddqjeqqpgofifmwvmcdjixjvjxrvyabqaqumfqiiqxizmhzevhxutsbgzcfggyyvolwaxfcpjhfpksxvgyxhddcssnxhygzvmyxrxqizzhpluxkautjmieximoskcffimctsfzgmihtoxkltopwobtfjvjymtuknxmsgevkeklprcaudidywwkfuhtatpeeiewczpwiegmpjquayfleczrvzekikbaeocpcurtxhcsysbbsyschxtrucpcoeabkikezvrzcelfyauqjpmgeiwpzcweieeptathufkwwydiduacrplkekvegsmxnkutmyjvjftbowpotlkxothimgzfstcmiffcksomixeimjtuakxulphzziqxrxymvzgyhxnsscddhxygvxskpfhjpcfxawlovyyggfczgbstuxhvezhmzixqiiqfmuqaqbayvrxjvjxijdcmvwmfifogpqqejqddrhyswvsuibuqiscepbotunodjwidthvpffjcpjpyktlsrjrywfrqqpmppzkhjzuumilwuzplmfftvmzkecmnhiousciebgjigtrpnxlqlnembofgsnurhzpwabi"
     s = Solution()
-    print(s.longestPalindrome(aa))
+    print(s.longestPalindrome3(aa))
     t2 = time.time()
     print(t2-t1)
     print(s.longestPalindrome1(aa))
     print(time.time()-t2)
-    print(s.longestPalindrome(aa)==s.longestPalindrome1(aa))
+    print(s.longestPalindrome3(aa)==s.longestPalindrome1(aa))
