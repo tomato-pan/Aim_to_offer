896b2d426ce641442bc349163639da68bd7e4b4d
panj <pj_uestc@163.com>
2022-01-04 16:09:29 +0800

    lc

---
+++ useAlgorothms/LC17.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        number = {'2': ['a', 'b', 'c'],
+                  '3': ['d', 'e', 'f'],
+                  '4': ['g', 'h', 'i'],
+                  '5': ['j', 'k', 'l'],
+                  '6': ['m', 'n', 'o'],
+                  '7': ['p', 'q', 'r', 's'],
+                  '8': ['t', 'u', 'v'],
+                  '9': ['w', 'x', 'y', 'z']}
+
+        def backtrack(conbination, nextdigit):
+            if len(nextdigit) == 0:
+                res.append(conbination)
+            else:
+                for letter in number[nextdigit[0]]:
+                    backtrack(conbination + letter, nextdigit[1:])
+
+        res = []
+        backtrack("", digits)
+        return res
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.letterCombinations("234"))
